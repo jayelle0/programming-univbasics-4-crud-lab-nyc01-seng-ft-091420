7011c39d9e86bb584eea8e78767b81693b073a2d
Jacqueline <lojacqueline2@gmail.com>
2020-08-28 03:05:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  element = "arrays!"
+   create_an_array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  element = 7
+  create_an_array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
