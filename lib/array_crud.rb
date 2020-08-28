1b331ccdbc170c5f1d03c5e5b0349c35efa2252d
Jacqueline <lojacqueline2@gmail.com>
2020-08-28 03:11:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,9 +12,10 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array (array, element)
+  array = 
   element = "wow"
-  create_an_array.unshift (element)
-  create_an_array
+  array.unshift (element)
+  array 
 end
 
 def remove_element_from_end_of_array(array)
